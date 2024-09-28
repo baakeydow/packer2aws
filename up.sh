ecf7ef76480c5002c3f7901a5e334139b0f0a544
baakeydow <baakey@21times2.com>
2024-09-28 23:58:17 +0200

    fix: use extended regex

--- up.sh
+++ up.sh
@@ -8,7 +8,7 @@ packer init .
 
 output=$(packer build . 2>&1)
 echo "$output"
-ami_id=$(echo "$output" | grep -oP '<sensitive>: ami-\w+' | cut -d' ' -f2)
+ami_id=$(echo "$output" | grep -oE '<sensitive>: ami-\w+' | cut -d' ' -f2)
 
 echo "$ami_id" > ami_id.txt
 echo "AMI created successfully with ID: $ami_id"
